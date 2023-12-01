870cda6af012e87505552fdbdb0b6f9b1694f7d4
Brent <brenton.bazemore@gmail.com>
2023-12-01 00:18:02 -0500

    Finish Day 1 - Part 2

--- days/1/2.ts
+++ days/1/2.ts
@@ -2,9 +2,26 @@ const inputFile = process.argv[2];
 const rawData = await Bun.file(`${import.meta.dir}/${inputFile || 'input.txt'}`).text();
 const data = rawData.split('\n');
 
+
+const numStrings: any = {
+  'one': 1,
+  'two': 2,
+  'three': 3,
+  'four': 4,
+  'five': 5,
+  'six': 6,
+  'seven': 7,
+  'eight': 8,
+  'nine': 9,
+}
+
 let sum = 0;
 data.forEach(line => {
-  const digits = line.replaceAll(/\D/g, '');
+  let clean = line;
+  Object.keys(numStrings).forEach(str => {
+    clean = clean.replaceAll(str, str[0] + numStrings[str] + str.slice(1));
+  });
+  const digits = clean.replaceAll(/\D/g, '');
   const first = digits[0];
   const last = digits.at(-1);
   const num = first + last;
