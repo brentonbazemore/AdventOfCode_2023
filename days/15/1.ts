4dfba1430ba43d5bd13ccdf0907d3b639ef18c73
Brent <brenton.bazemore@gmail.com>
2023-12-15 09:35:14 -0500

    Finish Day 15 - Part 1

--- days/15/1.ts
+++ days/15/1.ts
@@ -2,5 +2,16 @@ import '../../types/helper.d.ts';
 
 const inputFile = process.argv[2];
 const rawData = await Bun.file(`${import.meta.dir}/${inputFile || 'input.txt'}`).text();
-const data = rawData.split('\n');
+const data = rawData.split(',');
 
+let sum = 0;
+data.forEach(inst => {
+  let value = 0;
+  for (let i = 0; i < inst.length; i++) {
+    value += inst.charCodeAt(i);
+    value *= 17;
+    value %= 256;
+  }
+  sum += value;
+})
+console.log(sum);
