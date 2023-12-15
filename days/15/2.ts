ea81a8cb93d508b295b83760dea1dcba438e68ce
Brent <brenton.bazemore@gmail.com>
2023-12-15 10:43:39 -0500

    Finish Day 15 - Part 2

--- days/15/2.ts
+++ days/15/2.ts
@@ -4,14 +4,67 @@ const inputFile = process.argv[2];
 const rawData = await Bun.file(`${import.meta.dir}/${inputFile || 'input.txt'}`).text();
 const data = rawData.split(',');
 
-let sum = 0;
-data.forEach(inst => {
+// which box
+const hash = (inst: string) => {
   let value = 0;
   for (let i = 0; i < inst.length; i++) {
     value += inst.charCodeAt(i);
     value *= 17;
     value %= 256;
   }
-  sum += value;
+  return value;
+}
+
+// rn=1
+// rn = whicih lens
+// 1 = focal length
+
+// - means remove
+// = means 
+
+const boxes: { id: string, focal: number }[][] = [];
+for (let i = 0; i < 256; i++) {
+  boxes[i] = [];
+}
+
+data.forEach(inst => {
+  if (inst.includes('-')) {
+    const [lensId,] = inst.split('-');
+    const boxId = hash(lensId);
+    // console.log(lensId, boxId)
+    const lensPos = boxes[boxId].findIndex(lens => lens.id === lensId);
+    if (lensPos === -1) {
+      // do nothing
+    } else {
+      boxes[boxId].splice(lensPos, 1);
+    }
+  }
+  
+  if (inst.includes('=')) {
+    const [lensId, focal] = inst.split('=');
+    const boxId = hash(lensId);
+    // console.log(lensId, boxId)
+    const lensPos = boxes[boxId].findIndex(lens => lens.id === lensId);
+    if (lensPos === -1) {
+      boxes[boxId].push({ id: lensId, focal: +focal });
+    } else {
+      boxes[boxId][lensPos] = { id: lensId, focal: +focal };
+    }
+  }
 })
-console.log(sum);
+
+let power = 0;
+boxes.forEach((box, i) => {
+  if (box.length === 0) {
+    return;
+  }
+  const localPower = i + 1;
+  box.forEach((lens, j) => {
+    const slotNumber = j + 1;
+    const focalPower = localPower * lens.focal * slotNumber;
+    power += focalPower;
+    // console.log(`${lens.id}: ${localPower} (box ${i}) * ${slotNumber} (slot) * ${lens.focal} (focal length) = ${focalPower}`)
+  });
+})
+
+console.log(power);
