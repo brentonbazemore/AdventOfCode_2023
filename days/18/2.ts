166a532852c976ab15842520003b5a00fbdfb2fb
Brent <brenton.bazemore@gmail.com>
2023-12-19 21:58:46 -0500

    Finish Day 18 - Part 2

--- days/18/2.ts
+++ days/18/2.ts
@@ -4,211 +4,89 @@ const inputFile = process.argv[2];
 const rawData = await Bun.file(`${import.meta.dir}/${inputFile || 'input.txt'}`).text();
 const data = rawData.split('\n');
 
-const dugCoords: { [y: number]: { [x: number]: string }} = {}
-const filled: { [y: number]: { [x: number]: string }} = {}
-
-let count = 0;
-const display = () => {
-  // const output = Bun.file('./days/18/output.txt');
-  // const writer = output.writer();
-  let minY = Infinity;
-  let maxY = -Infinity;
-  let minX = Infinity;
-  let maxX = -Infinity;
-  Object.typedKeys(dugCoords).forEach(y => {
-    minY = Math.min(minY, y);
-    maxY = Math.max(maxY, y);
-    Object.typedKeys(dugCoords[y]).forEach(x => {
-      minX = Math.min(minX, x);
-      maxX = Math.max(maxX, x);
-    });
-  });
-
-  for (let y = minY; y <= maxY; y++) {
-    let row = '';
-    for (let x = minX; x <= maxX; x++) {
-      if (dugCoords[y]?.[x] || filled[y]?.[x]) {
-        row += '#'
-        count++;
-      } else {
-        row += '.'
-      }
-    }
-    // writer.write(row + '\n');
-    console.log(row);
-  }
-  console.log(count);
-}
-
-// const findCorners = (start: [number, number]) => {
-//   let minY = Infinity;
-//   let maxY = -Infinity;
-//   let minX = Infinity;
-//   let maxX = -Infinity;
-//   Object.typedKeys(dugCoords).forEach(y => {
-//     minY = Math.min(minY, y);
-//     maxY = Math.max(maxY, y);
-//     Object.typedKeys(dugCoords[y]).forEach(x => {
-//       minX = Math.min(minX, x);
-//       maxX = Math.max(maxX, x);
-//     });
-//   });
-
-//   const points: [number, number][] = [];
-
-//   let currCorner = start;
-
-//   for (let y = minY; y <= maxY; y++) {
-//     for (let x = minX; x <= maxX; x++) {
-//       const hasVert = dugCoords[y - 1]?.[x] != null || dugCoords[y + 1]?.[x] != null;
-//       const hasHori = dugCoords[y]?.[x - 1] != null || dugCoords[y]?.[x + 1] != null;
-      
-//       if (dugCoords[y]?.[x]) {
-//         if (hasHori && hasVert) {
-//           points.push([x, y]);
-//         }
-//       }
-//     }
-//   }
-
-//   return points;
-// }
 
 enum Direction {
   RIGHT = 'R',
-  UP = 'U',
   DOWN = 'D',
-  LEFT = 'L'
+  LEFT = 'L',
+  UP = 'U',
 }
+
+const num2enum: any = {
+  0: Direction.RIGHT,
+  1: Direction.DOWN,
+  2: Direction.LEFT,
+  3: Direction.UP
+}
+
+
+const instructions = data.map((line) => {
+  const [,rawHex] = line.split('#');
+  const cleanHex = rawHex.replace(')', '');
+  const num = parseInt(cleanHex.substring(0, 5), 16);
+  const direction = num2enum[+cleanHex.at(5)!] as Direction;
+  return { direction, num }
+})
+
+// console.log(inst);
 
 let position = { x: 0, y: 0 }
-const dig = (x: number, y: number, color: string) => {
-  dugCoords[y] = dugCoords[y] ?? {};
-  dugCoords[y][x] = color;
-}
 
-dig(position.x, position.y, 'none')
-// const corners: [number, number][] = [[position.x, position.y]];
-data.forEach(line => {
-  const [direction, rawLength, rawColor] = line.split(' ');
-  const length = +rawLength;
-  const color = rawColor.replace('(', '').replace(')', '');
-
+let perimeter = 0;
+const corners: { x: number, y: number }[] = [{ x: position.x, y: position.y }];
+instructions.forEach(({ direction, num: length }) => {
   if (direction === Direction.RIGHT) {
-    for (let i = 0; i < length; i++) {
-      dig(position.x, position.y, color);
-      position.x += 1;
-    }
+    position.x += length;
   }
   if (direction === Direction.LEFT) {
-    for (let i = 0; i < length; i++) {
-      dig(position.x, position.y, color);
-      position.x -= 1;
-    }
+    position.x -= length;
   }
   if (direction === Direction.UP) {
-    for (let i = 0; i < length; i++) {
-      dig(position.x, position.y, color);
-      position.y -= 1;
-    }
+    position.y -= length;
   }
   if (direction === Direction.DOWN) {
-    for (let i = 0; i < length; i++) {
-      dig(position.x, position.y, color);
-      position.y += 1;
-    }
+    position.y += length;
   }
-  // corners.push([position.x, position.y]);
+
+  perimeter += length;
+  // 1. Write down the coords of the vertices of the irregular polygon
+  corners.push({ x: position.x, y: position.y });
 });
+corners.pop();
 
-const findAPointInside = () => {
-  let minY = Infinity;
-  let maxY = -Infinity;
-  let minX = Infinity;
-  let maxX = -Infinity;
-  Object.typedKeys(dugCoords).forEach(y => {
-    minY = Math.min(minY, y);
-    maxY = Math.max(maxY, y);
-    Object.typedKeys(dugCoords[y]).forEach(x => {
-      minX = Math.min(minX, x);
-      maxX = Math.max(maxX, x);
-    });
-  });
+// 2. Create an array (in counter clockwise order)
+// corners.reverse(); // not sure which way it goes...
 
-  for (let x = minX; x <= maxX; x++) {
-    if (dugCoords[minY][x]) {
-      if (!dugCoords[minY + 1][x]) {
-        return [x, minY + 1] as [number, number];
-      }
-    }
-  }
+// 3. Multiply the x coordinate of each vertex by the y coordinate of the next vertex.
+// let step3 = 0;
+let step5 = 0;
+for (let i = 0; i < corners.length; i++) {
+  const x = corners.at(i)!.x;
+  const y = corners.at((i + 1) % corners.length)!.y;
+
+  // Have to add and subtract 3. and 4. at the same time because the numbers get too big -_- 
+  // -11566665988249564
+  // -11692190884120664
+  const x2 = corners.at((i + 1) % corners.length)!.x;
+  const y2 = corners.at(i)!.y;
+  step5 += x * y;
+  step5 -= x2 * y2;
 }
 
-const isEdge = (x: number, y: number) => {
-  return !!dugCoords[y]?.[x];
-}
+// 4. Multiply the y coordinate of each vertex by the x coordinate of the next vertex.
+// let step4 = 0;
+// for (let i = 0; i < corners.length; i++) {
+//   const x = corners.at((i + 1) % corners.length)!.x;
+//   const y = corners.at(i)!.y;
+  
+//   step4 += x * y;
+// }
 
-const checkAdjacent = (x: number, y: number) => {
-  const anEdge = isEdge(x, y);
-  const isFilled = filled[y]?.[x];;
+// console.log(step3)
+// console.log(step4);
+// // 5. Subtract the sum of the second products from the sum of the first products.
+// let step5 = step3 - step4;
 
-  return !anEdge && !isFilled;
-}
-
-const addFilled = (x: number, y: number) => {
-  filled[y] = filled[y] ?? {};
-  filled[y][x] = 'filled';
-}
-
-const fill = (x: number, y: number) => {
-  const queue: [number, number][] = [];
-
-  queue.push([x, y]);
-  filled[y] = filled[y] ?? {};
-  filled[y][x] = 'filled';
-
-  while (queue.length > 0) {
-    const [curX, curY] = queue.shift()!;
-    if (isEdge(curX, curY)) {
-      return false;
-    }
-
-    // up
-    if (checkAdjacent(curX, curY - 1)) {
-      addFilled(curX, curY - 1);
-      queue.push([curX, curY - 1]);
-    }
-    // down
-    if (checkAdjacent(curX, curY + 1)) {
-      addFilled(curX, curY + 1)
-      queue.push([curX, curY + 1]);
-    }
-    // left
-    if (checkAdjacent(curX - 1, curY)) {
-      addFilled(curX - 1, curY)
-      queue.push([curX - 1, curY]);
-    }
-    // right
-    if (checkAdjacent(curX + 1, curY)) {
-      addFilled(curX + 1, curY)
-      queue.push([curX + 1, curY]);
-    }
-  }
-
-  return true;
-
-  // console.log(filled);
-}
-
-const start = findAPointInside()!;
-fill(start[0], start[1]);
-
-
-// corners.pop(); // puts start in twice
-// console.log(corners)
-
-// // const corners = findCorners();
-// // console.log(corners)
-// console.log(pip.pointInPolyWindingNumber(corners, [1, 1]))
-
-display();
+// 6. Divide this difference by 2 to get the area of the polygon.
+console.log((((step5 + perimeter) / 2) + 1));
+// console.log(perimeter);
