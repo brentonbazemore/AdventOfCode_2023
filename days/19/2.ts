a9c332ec124df8da68a3032473f2dc25bfe085f6
Brent <brenton.bazemore@gmail.com>
2023-12-20 22:54:27 -0500

    Finish Day 19 - Part 2

--- days/19/2.ts
+++ days/19/2.ts
@@ -34,60 +34,125 @@ rawWorkFlow.split('\n').forEach(line => {
   workflows.set(name, { name, steps });
 });
 
-const parts = rawParts.split('\n').map(line => {
-  const rawPairs = line.replaceAll(/[{}]/g, '').split(',');
-  const part: { [variable: string]: number } = {}
-  rawPairs.forEach(rp => {
-    const [variable, rawAmount] = rp.split('=');
-    const amount = +rawAmount;
-    part[variable] = amount;
-  });
-  return part;
-});
+// const parts = rawParts.split('\n').map(line => {
+//   const rawPairs = line.replaceAll(/[{}]/g, '').split(',');
+//   const part: { [variable: string]: number } = {}
+//   rawPairs.forEach(rp => {
+//     const [variable, rawAmount] = rp.split('=');
+//     const amount = +rawAmount;
+//     part[variable] = amount;
+//   });
+//   return part;
+// });
 
+// const comparators = {
+//   '>': (validAmount: [number, number], stepAmount: number): [number, number] | null => {
+//     if (validAmount[1] < stepAmount) {
+//       return null;
+//     }
 
-const comparators = {
-  '>': (partAmount: number, workflowAmount: number) => partAmount > workflowAmount,
-  '<': (partAmount: number, workflowAmount: number) => partAmount < workflowAmount,
-}
+//     if (validAmount[0] <= stepAmount) {
+//       return [stepAmount + 1, validAmount[1]];
+//     }
 
-const acceptedParts = parts.filter(part => {
-  let workflow = 'in';
+//     return validAmount;
+//   },
+//   '<': (validAmount: [number, number], stepAmount: number): [number, number] | null => {
+//     if (validAmount[0] > stepAmount) {
+//       return null;
+//     }
 
-  let sorted = '';
-  while (!sorted) {
-    // console.log(workflow);
-    if (workflow === 'A') {
-      sorted = 'accepted';
+//     if (validAmount[1] >= stepAmount) {
+//       return [validAmount[0], stepAmount - 1];
+//     }
+
+//     return validAmount;
+//   },
+// }
+
+type Range = { lower: number, upper: number };
+
+const splitWorkflow = (workflowId: string, inputs: { x: Range, m: Range, a: Range, s: Range }) => {
+  if (workflowId === 'A') {
+    // console.log(inputs)
+    const piece = ((inputs.x.upper - inputs.x.lower) + 1) 
+          * ((inputs.m.upper - inputs.m.lower) + 1) 
+          * ((inputs.a.upper - inputs.a.lower) + 1) 
+          * ((inputs.s.upper - inputs.s.lower) + 1);
+    // console.log(piece);
+    return piece;
+    
+  }
+
+  if (workflowId === 'R') {
+    return 0;
+  }
+
+  let newInputs: typeof inputs = JSON.parse(JSON.stringify(inputs));
+  const steps = workflows.get(workflowId)!.steps;
+  let validAmount = 0;
+  for (let i = 0; i < steps.length; i++) {
+    const step = steps[i];
+    if (step.variable === NO_OP) {
+      validAmount += splitWorkflow(step.destination, newInputs);
       break;
     }
-    if (workflow === 'R') {
-      sorted = 'rejected';
-      break;
-    }
 
-    const steps = workflows.get(workflow)!.steps;
-    for (let i = 0; i < steps.length; i++) {
-      const step = steps[i];
-      if (step.variable === NO_OP) {
-        workflow = step.destination;
-        break;
-      }
-  
-      // console.log(part[step.variable], step.comparator, step.amount)
-      if (comparators[step.comparator as '>' | '<'](part[step.variable], step.amount)) {
-        workflow = step.destination;
-        break;
-      }
+    if (step.comparator === '>') {
+      validAmount += splitWorkflow(step.destination, { ...newInputs, [step.variable]: { lower: step.amount + 1, upper: newInputs[step.variable as 'x'].upper }});
+      newInputs = { ...newInputs, [step.variable]: { lower: newInputs[step.variable as 'x'].lower, upper: step.amount }};
+    }
+    if (step.comparator === '<') {
+      validAmount += splitWorkflow(step.destination, { ...newInputs, [step.variable]: { lower: newInputs[step.variable as 'x'].lower, upper: step.amount - 1 }});
+      newInputs = { ...newInputs, [step.variable]: { lower: step.amount, upper: newInputs[step.variable as 'x'].upper }};
     }
   }
 
-  // console.log(sorted);
-  return sorted === 'accepted';
-});
+  return validAmount;
+}
 
-let sum = 0;
-acceptedParts.forEach(part => sum += part.x + part.m + part.a + part.s);
-console.log(sum);
+const out = splitWorkflow('in', { x: { lower: 1, upper: 4000 }, m: { lower: 1, upper: 4000 }, a: { lower: 1, upper: 4000 }, s: { lower: 1, upper: 4000 } });
+console.log(out);
+
+// console.log(accepted);
+// 
+// const acceptedParts = parts.filter(part => {
+//   let workflow = 'in';
+
+//   let sorted = '';
+//   while (!sorted) {
+//     // console.log(workflow);
+//     if (workflow === 'A') {
+//       sorted = 'accepted';
+//       break;
+//     }
+//     if (workflow === 'R') {
+//       sorted = 'rejected';
+//       break;
+//     }
+
+//     const steps = workflows.get(workflow)!.steps;
+//     for (let i = 0; i < steps.length; i++) {
+//       const step = steps[i];
+//       if (step.variable === NO_OP) {
+//         workflow = step.destination;
+//         break;
+//       }
+  
+//       // console.log(part[step.variable], step.comparator, step.amount)
+//       if (comparators[step.comparator as '>' | '<'](part[step.variable as 'x'], step.amount)) {
+//         workflow = step.destination;
+//         break;
+//       }
+//     }
+//   }
+
+//   // console.log(sorted);
+//   return sorted === 'accepted';
+// });
+
+// let sum = 0;
+// acceptedParts.forEach(part => sum += part.x + part.m + part.a + part.s);
+// console.log(sum);
 
 // console.log(workflows);
